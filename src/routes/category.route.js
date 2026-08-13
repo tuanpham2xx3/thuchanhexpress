c4d965c3fee4529ae3cc064869538b7bef6689da
Tuna Phạm <134152079+tuanpham2xx3@users.noreply.github.com>
2026-08-13 21:17:53 +0700

    test search category

--- src/routes/category.route.js
+++ src/routes/category.route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { createCategory } from "../controllers/category.controller.js";
+import { getCategories } from "../controllers/category.controller.js";
 
 const router = express.Router();
 
