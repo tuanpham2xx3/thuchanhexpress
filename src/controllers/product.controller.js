9bb6d1fad242b82ec591e33f2973236d63d70c83
Tuna Phạm <134152079+tuanpham2xx3@users.noreply.github.com>
2026-08-13 22:14:20 +0700

    impl get by id category

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -97,3 +97,42 @@ export const getProducts = async (req, res) => {
     });
   }
 };
+
+export const getProductById = async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+
+    if (isNaN(id)) {
+      return res.status(400).json({
+        message: "Invalid product id",
+      });
+    }
+
+    const product = await prisma.product.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        categories: {
+          include: {
+            category: true,
+          },
+        },
+      },
+    });
+
+    if (!product) {
+      return res.status(404).json({
+        message: "Product not found",
+      });
+    }
+
+    return res.status(200).json(product);
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      message: "Server error",
+    });
+  }
+};
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   createProduct,
   getProducts,
+  getProductById,
 } from "../controllers/product.controller.js";
 
 const router = express.Router();
 
 router.post("/", createProduct);
 router.get("/", getProducts);
+router.get("/:id", getProductById);
 
 export default router;
