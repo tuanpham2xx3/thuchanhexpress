4a11a3bb33f40f29e22700e2c6c77ee3cbef2367
Tuna Phạm <134152079+tuanpham2xx3@users.noreply.github.com>
2026-08-17 14:35:45 +0700

    impl update product

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -168,3 +168,100 @@ export const getProductById = async (req, res) => {
     });
   }
 };
+
+export const updateProduct = async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    const { name, price, categoryIds } = req.body;
+
+    if (isNaN(id)) {
+      return res.status(400).json({
+        message: "Invalid product id",
+      });
+    }
+
+    const productExist = await prisma.product.findUnique({
+      where: { id },
+    });
+
+    if (!productExist) {
+      return res.status(404).json({
+        message: "Product not found",
+      });
+    }
+
+    if (!name) {
+      return res.status(400).json({
+        message: "Name is required",
+      });
+    }
+
+    const numericPrice = Number(price);
+
+    if (Number.isNaN(numericPrice) || numericPrice < 0) {
+      return res.status(400).json({
+        message: "Invalid price",
+      });
+    }
+
+    if (!Array.isArray(categoryIds) || categoryIds.length === 0) {
+      return res.status(400).json({
+        message: "categoryIds is required",
+      });
+    }
+
+    const categories = await prisma.category.findMany({
+      where: {
+        id: {
+          in: categoryIds,
+        },
+      },
+    });
+
+    if (categories.length !== categoryIds.length) {
+      return res.status(400).json({
+        message: "One or more categories do not exist",
+      });
+    }
+
+    const updatedProduct = await prisma.product.update({
+      where: { id },
+
+      data: {
+        name,
+        price: numericPrice,
+
+        categories: {
+          // Xóa quan hệ category cũ
+          deleteMany: {},
+
+          // Tạo lại quan hệ category mới
+          create: categoryIds.map((categoryId) => ({
+            category: {
+              connect: {
+                id: categoryId,
+              },
+            },
+          })),
+        },
+      },
+
+      include: {
+        categories: {
+          include: {
+            category: true,
+          },
+        },
+      },
+    });
+
+    return res.status(200).json(updatedProduct);
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      message: "Server error",
+    });
+  }
+};
+
