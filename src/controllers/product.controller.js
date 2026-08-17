733c164dce2a08391cb4e1526d4f06e36a03d472
Tuna Phạm <134152079+tuanpham2xx3@users.noreply.github.com>
2026-08-17 14:16:09 +0700

    impl pagination product

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -77,14 +77,23 @@ export const getProducts = async (req, res) => {
   try {
     const { search } = req.query;
 
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 5;
+
+    const skip = (page - 1) * limit;
+
+    const where = search
+      ? {
+          name: {
+            contains: search,
+          },
+        }
+      : {};
+
     const products = await prisma.product.findMany({
-      where: search
-        ? {
-            name: {
-              contains: search,
-            },
-          }
-        : {},
+      where,
+      skip,
+      take: limit,
       include: {
         categories: {
           include: {
@@ -97,7 +106,21 @@ export const getProducts = async (req, res) => {
       },
     });
 
-    return res.status(200).json(products);
+    const total = await prisma.product.count({
+      where,
+    });
+
+    const totalPages = Math.ceil(total / limit);
+
+    return res.status(200).json({
+      data: products,
+      pagination: {
+        page,
+        limit,
+        total,
+        totalPages,
+      },
+    });
   } catch (error) {
     console.log(error);
 
